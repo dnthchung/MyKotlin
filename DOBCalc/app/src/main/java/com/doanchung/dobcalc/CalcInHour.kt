3b60b9ce0b61dabc702b1139bcabeb51002a391b
chunChu <doanthanhchung03nb@gmail.com>
2024-03-01 13:22:56 +0700

    update dob cal

---
+++ DOBCalc/app/src/main/java/com/doanchung/dobcalc/CalcInHour.kt
@@ -0,0 +1,11 @@
+package com.doanchung.dobcalc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class CalcInHour : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
