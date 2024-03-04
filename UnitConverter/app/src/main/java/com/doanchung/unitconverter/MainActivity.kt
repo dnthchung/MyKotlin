ae335b1590a03653f8f3c7a1a0ce38382f85fe3a
chunChu <doanthanhchung03nb@gmail.com>
2024-03-04 11:24:40 +0700

    add toast

--- UnitConverter/app/src/main/java/com/doanchung/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/doanchung/unitconverter/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.doanchung.unitconverter
 
+import android.content.res.Resources.Theme
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.doanchung.unitconverter.ui.theme.UnitConverterTheme
 
@@ -37,27 +42,29 @@ class MainActivity : ComponentActivity() {
 fun UnitConverter(){
     Column {
         //here all the ui ele will be stacked bellow
-        Greeting("Android")
-        Greeting("Android")
-
+        Text(text = "Unit Converter : ")
+        TextField(value = "", onValueChange = {
+            /**
+             * here goes what should happen, when the value of OutlineTextFiled changes
+             */
+        })
         Row {
-
+            val context = LocalContext.current
+            Button(onClick = {
+                Toast.makeText(context,
+                    "clicked",
+                    Toast.LENGTH_LONG).show()
+            }) {
+                Text(text = "Click Me")
+            }
         }
+        Text(text = "Result : ")
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UnitConverterTheme {
-        Greeting("Android")
-    }
+fun UnitConverterPreview(){
+    UnitConverter()
 }
+
