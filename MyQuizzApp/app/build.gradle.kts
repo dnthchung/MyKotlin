a49acdaeadc1d6d3124c3764f41287b709864637
chunChu <doanthanhchung03nb@gmail.com>
2024-03-12 11:34:05 +0700

    Creating the question Model And Preparing the questions
    - bất kì img nào cũng dc rt về int

--- MyQuizzApp/app/build.gradle.kts
+++ MyQuizzApp/app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3-android:1.2.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 //    implementation("androidx.compose.material3:material3:1.1.0-alpha01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- MyQuizzApp/app/src/main/java/com/doanchung/myquizzapp/MainActivity.kt
+++ MyQuizzApp/app/src/main/java/com/doanchung/myquizzapp/MainActivity.kt
@@ -1,46 +1,34 @@
- package com.doanchung.myquizzapp
+package com.doanchung.myquizzapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.doanchung.myquizzapp.ui.theme.MyQuizzAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyQuizzAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.activity_main)
+
+
+        val btnStart: Button = findViewById(R.id.btn_start)
+        val etName: EditText = findViewById(R.id.et_name)
+        btnStart.setOnClickListener {
+
+            if (etName.text.isEmpty()) {
+                Toast.makeText(this,
+                    "Please enter ur name.", Toast.LENGTH_LONG).show()
+            }else{
+                //create intent(đi từ đâu ? - từ this activity , đến đâu ? - quiz activity
+                val intent = Intent(this,QuizQuestionsActivity::class.java)
+                startActivity(intent)
+
+                //bắt nhập lại tên, close cái activity main đã mở trc đó
+                finish()
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyQuizzAppTheme {
-        Greeting("Android")
     }
 }
