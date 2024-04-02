fa441d0ee24881316dffb0ab19a337ed1cbc42dd
chunChu <doanthanhchung03nb@gmail.com>
2024-04-02 14:49:00 +0700

    complete cart

--- ShoesShopEcommerce/app/src/main/java/com/doanchung/shoes_shop_ecommerce/activity/MainActivity.kt
+++ ShoesShopEcommerce/app/src/main/java/com/doanchung/shoes_shop_ecommerce/activity/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
+import com.doanchung.shoes_shop_ecommerce.adapter.PopularAdapter
 import com.doanchung.shoes_shop_ecommerce.adapter.SliderAdapter
 import com.doanchung.shoes_shop_ecommerce.databinding.ActivityMainBinding
 import com.doanchung.shoes_shop_ecommerce.model.SliderModel
 import com.doanchung.shoes_shop_ecommerce.viewmodel.MainViewModel
+import com.example.project1763.Adapter.BrandAdapter
 
 class MainActivity : BaseActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -50,5 +54,24 @@ class MainActivity : BaseActivity() {
             binding.dotIndicator.attachTo(binding.viewpagerSlider)
         }
     }
+    private fun initBrand() {
+        binding.progressBarBrand.visibility = View.VISIBLE
+        viewModel.brands.observe(this, Observer {
+            binding.viewBrand.layoutManager =
+                LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
+            binding.viewBrand.adapter = BrandAdapter(it)
+            binding.progressBarBrand.visibility = View.GONE
+        })
+        viewModel.loadBrand()
+    }
 
+    private fun initPopular() {
+        binding.progressBarPopular.visibility = View.VISIBLE
+        viewModel.popular.observe(this, Observer {
+            binding.viewPopular.layoutManager = GridLayoutManager(this@MainActivity, 2)
+            binding.viewPopular.adapter = PopularAdapter(it)
+            binding.progressBarPopular.visibility = View.GONE
+        })
+        viewModel.loadPupolar()
+    }
 }
